872c356ccde68c599b04c09f1f7b05fb12fd640a
maparce <javier.arce.nazario@gmail.com>
2020-12-28 09:35:14 -0500

    working rectangle animation with animation line and pointer line

--- js/xy_move_SVG_rectangle.js
+++ js/xy_move_SVG_rectangle.js
@@ -7,8 +7,9 @@
 
 function addPointer(SVGid){
    daLine = document.getElementById(SVGid)
-   daLine.style.pointerEvents = "stroke"
-   daLine.style.cursor = "ew-resize"
+   daLine.style.pointerEvents = "stroke";
+   daLine.style.cursor = "ew-resize";
+   daLine.style.strokeWidth = "3%";
    daLine.setAttributeNS(null,"onmousedown","onMouseDown(evt)");
    daLine.setAttributeNS(null,"onmouseover","onMouseOverAni()");
    daLine.setAttributeNS(null,"onmouseout", "onMouseOverOut()");
@@ -27,27 +28,36 @@ var mouseOver = 0
 window.addEventListener("mouseup", onMouseUp);
 function onMouseOverAni(){
 if (mouseOver == 0) {
-   document.getElementById('daLine').style.animation = "none"
-   document.getElementById('daLine').style.strokeWidth = "3%"
-   document.getElementById('daLine').style.opacity = "1"
+   // document.getElementById('daLineAni').style.animation = "none"
+   document.getElementById('daLineAni').style.animation = "aniLine .75s linear"
+   document.getElementById('daLineAni').style.strokeWidth = "3%"
+   document.getElementById('daLineAni').style.opacity = "1"
+   
    document.getElementById("container").append(daSVG) // to reactivate the animation
    console.log(mouseOver)
    mouseOver = 1
 
    }
 else {
-   document.getElementById('daLine').style.opacity = "0";
+   document.getElementById('daLineAni').style.opacity = "0";
    document.getElementById("container").append(daSVG);
 }
    // document.getElementById('daLine').style.opacity = "0"
 };
 
  function onMouseOverOut() { 
-   document.getElementById('daLine').style.opacity = "0"
-   document.getElementById('daLine').style.animation = "aniLine .75s linear reverse"
-   document.getElementById("container").append(daSVG)
+   
+   if (bmousedown == 0 && mouseOver == 1){
+      document.getElementById('daLineAni').style.animation = "aniLine .75s linear reverse"
+      document.getElementById('daLineAni').style.strokeWidth = "0%"
+      document.getElementById("container").append(daSVG)
+      mouseOver = 0   
+   } 
+      else {
+   document.getElementById('daLineAni').style.opacity = "0"}
    mouseOver = 0
    }
+
 
 
 function getMouse(evt){
@@ -108,10 +118,12 @@ function getMouse(evt){
    //  console.log(newPX)
     if (newPX < 500 && newPX > 0) {
     document.getElementsByClassName("clipy")[0].style.clipPath = "inset(0 " + newPercent +"% 0 0)"
-    document.getElementById('daLine').setAttributeNS(null, 'x1', newPX)
-    document.getElementById('daLine').setAttributeNS(null, 'x2', newPX)
-    document.getElementById('daLine').style.opacity = "0"
-    document.getElementById('daLine').style.animation = "aniLine .75s linear reverse"
+    document.getElementById('daLineBack').setAttributeNS(null, 'x1', newPX)
+    document.getElementById('daLineBack').setAttributeNS(null, 'x2', newPX)
+    document.getElementById('daLineAni').setAttributeNS(null, 'x1', newPX)
+    document.getElementById('daLineAni').setAttributeNS(null, 'x2', newPX)
+    document.getElementById('daLineAni').style.opacity = "0"
+    document.getElementById('daLineAni').style.animation = "aniLine .75s linear reverse"
     document.getElementById("container").append(document.getElementById('map1937'))
     document.getElementById("container").append(daSVG) // to reactivate the animation
     mouseOver = 2
